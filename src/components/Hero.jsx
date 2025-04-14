85c36b030712e2f480d52e79e9203842007852f4
Rangga Mukti <r4n994mukti@gmail.com>
2025-04-14 12:14:50 +0700

    Create Button.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,5 @@
+import { ButtonPrimary, ButtonOutline } from './Button';
+
 function Hero() {
   return (
     <section id="home" className="pt-28 lg:pt-36">
@@ -15,7 +17,10 @@ function Hero() {
             </div>
           </div>
           <h2 className="headline-1 max-w-[15ch] sm:max-w-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10">Building Scalable Modern Websites for the Future</h2>
-          <div className="flex items-center gap-3">Button Primary ButtonOutline</div>
+          <div className="flex items-center gap-3">
+            <ButtonPrimary label="Download CV" icon="download" />
+            <ButtonOutline href="#about" label="Scroll down" icon="arrow_downward" />
+          </div>
         </div>
         <div className="hidden lg:block">
           <figure className="w-full max-w-[480px] ml-auto bg-gradient-to-t from-sky-400 via-25% via-sky-400/40 to-65% rounded-[60px] overflow-hidden">
