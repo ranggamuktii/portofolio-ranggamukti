f76a0813e11c1b8d972d50d8a6cac9a1f48ef6d4
Rangga Mukti <r4n994mukti@gmail.com>
2025-04-14 15:47:50 +0700

    Create About.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import About from './components/About';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <main>
         <Hero />
+        <About />
       </main>
     </>
   );
