8d3986b5867c9cdb4daca0853ebdbf221e14af6a
Rangga Mukti <r4n994mukti@gmail.com>
2025-04-15 01:15:47 +0700

    CREATE Contact.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from './components/About';
 import Skill from './components/Skill';
 import Work from './components/Work';
 import Review from './components/Review';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Skill />
         <Work />
         <Review />
+        <Contact />
       </main>
     </>
   );
