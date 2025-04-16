8d3343dc3599bd935ff9b1e7e26b47805d165659
Rangga Mukti <r4n994mukti@gmail.com>
2025-04-16 10:58:38 +0700

    UPDATE margin-top navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,10 @@
-// import { useState } from 'react';
 import Navbar from './Navbar';
 
 function Header() {
-  // const [navOpen] = useState(true);
-
   return (
     <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
       <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-center items-center">
-        <div className="w-full flex justify-center">
+        <div className="w-full flex justify-center mt-10">
           <Navbar />
         </div>
       </div>
